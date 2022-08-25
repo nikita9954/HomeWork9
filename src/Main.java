808a17bf95f0251644d6225a15d14ea793cabfac
nikita9954 <kunitsa.nikita2@gmail.com>
2022-08-26 00:11:57 +0300

    Задание на ошибки

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Metods metods = new Metods();
+        metods.setA(scanner.nextInt());
+        metods.getA();
+        metods.metod();
+        metods.metod1();
+        metods.metod2();
+        metods.metod3();
+    }
+}
+
+
+
+
+
---
+++ src/Metods.java
@@ -0,0 +1,66 @@
+public class Metods {
+    private int a;
+    private int d;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void metod() {
+        try {
+            d = a / 0;
+            System.out.println("Текст появился код неверный");
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на ноль словили ошибку");
+        }
+        System.out.println("Cловил ошибку круто ");
+        System.out.println("Первый блок ошибка простая через try catch");
+    }
+
+    public void metod1() {
+        try {
+            d = a / 1;
+            int[] array = new int[a];
+            array[a + 1] = d;
+            System.out.println("Текст появился код неверный");
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на ноль словили ошибку");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Неправильный индекс");
+        }
+        System.out.println("Cделали 2 кетча   круто ");
+    }
+
+    public void metod2() {
+        try {
+            d = a / 1;
+            int[] array = new int[a];
+            array[a + 1] = d;
+            System.out.println("Текст появился код неверный");
+        } catch (ArithmeticException | IndexOutOfBoundsException e) {
+            System.out.println("Споймали ошибку c помощью мульти кетча");
+        }
+    }
+
+    public void metod3() {
+        try {
+            d = a / 1;
+            int[] array = new int[a];
+            array[a + 1] = d;
+            System.out.println("Текст появился код неверный");
+        } catch (ArithmeticException | IndexOutOfBoundsException e) {
+            System.out.println("Споймали ошибку c помощью мульти кетча должен еще появится блок файнали");
+        } finally {
+            System.out.println("Этот текст будет в любом случае");
+        }
+    }
+}
+
+
+
+
+
